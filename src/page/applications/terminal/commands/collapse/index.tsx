9d21c944da69f4787a2b2f0b3ddf70e667a5f3bf
Vladya5297 <123dobro@mail.ru>
2023-08-11 23:50:07 +0300

    feat: collapse command refactored

--- src/page/applications/terminal/commands/collapse/index.tsx
+++ src/page/applications/terminal/commands/collapse/index.tsx
@@ -1,6 +1,8 @@
 import {createPortal} from 'react-dom';
+import {useEffect} from 'react';
 
 import {ROOT_ID} from '~/constants/root';
+import {resetFocus} from '~/utils/dom';
 
 import type {Command} from '../types';
 import {key} from '../../utils/key';
@@ -8,12 +10,20 @@ import {key} from '../../utils/key';
 import css from './style.m.css';
 
 const Collapse = () => {
-    const root = document.getElementById(ROOT_ID)!;
-    root.inert = true;
+    useEffect(resetFocus, []);
 
     return createPortal(
-        <div className={css.collapse} />,
-        root,
+        <div className={css.collapse} onClick={event => {event.stopPropagation()}}>
+            <h3>Blue screen of death</h3>
+            <div>
+                <p>A fatal exception 0E has occurred at 0028:C1101AC7 in UXD ctpc19x(05)</p>
+                <p>You've broke everything, now you unable to continue to use the system.</p>
+                <p>It's all your fault. There is no way to fix it.</p>
+                <p>The only thing you can do - contact the administrator:</p>
+                <a href="mailto:1257dobro@gmail.com">1257dobro@gmail.com</a>
+            </div>
+        </div>,
+        document.getElementById(ROOT_ID)!,
     );
 };
 
